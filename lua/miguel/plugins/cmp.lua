95c95e6324c8a3ee5a8dde1e2bb67ead3d52d9d8
Miguel Sarasa <miguel.sarasa@dieffenbacher.de>
2025-03-28 09:15:08 +0100

    fix disabling insert completion on external commands

--- lua/miguel/plugins/cmp.lua
+++ lua/miguel/plugins/cmp.lua
@@ -1,31 +1,3 @@
--- return {
---     "hrsh7th/nvim-cmp",
---     event = "InsertEnter",
---     dependencies = {
---         "hrsh7th/cmp-buffer", -- source for text in buffer
---         "hrsh7th/cmp-path",   -- source for file system paths
---     },
---     config = function()
---         local cmp = require("cmp")
---         cmp.setup({
---             mapping = cmp.mapping.preset.insert({
---                 ["<C-d>"] = cmp.mapping.scroll_docs(-4),
---                 ["<C-u>"] = cmp.mapping.scroll_docs(4),
---                 ["<C-Space>"] = cmp.mapping.complete(),
---                 ["<C-e>"] = cmp.mapping.close(),
---                 ["<C-y>"] = cmp.mapping.confirm({
---                     behavior = cmp.ConfirmBehavior.Replace,
---                     select = true,
---                 }),
---             }),
---                 sources = cmp.config.sources({
---                     { name = "nvim_lsp" },
---                     { name = "path" },
---                 })
---             })
---     end
--- }
---
 return {
     {
         'saghen/blink.cmp',
@@ -34,12 +6,6 @@ return {
         version = 'v0.*',
 
         opts = {
-            enabled = function ()
-                local disable = vim.fn.mode() == 'c' and vim.fn.getcmdline():find("!") -- no autocomplete for external commands because WSL
-                    or vim.bo.filetype == "TelescopePrompt" -- and no autocomplete for telescope because why would there be
-                vim.print(string.format("blink.cmp enable callback: returning %q, mode is %q", not disable, vim.fn.mode()))
-                return not disable
-            end,
             keymap = {
                 preset = 'default',
                 ["<C-u>"] = { "scroll_documentation_up", "fallback" },
@@ -51,9 +17,15 @@ return {
                 nerd_font_variant = 'mono'
             },
             signature = { enabled = true },
-            -- sources = {
-            --     default = {'lsp', 'snippets', 'buffer'} --path is out because wsl
-            -- }
+            sources = {
+                providers = {
+                    cmdline = {
+                        enabled = function ()
+                            return vim.fn.getcmdtype() ~= ':' or not vim.fn.getcmdline():find('!')
+                        end
+                    }
+                }
+            }
         },
     },
 }
