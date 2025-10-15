0399c0096b4530c2c86f0e0e3ad29f799ad3826d
Miguel Sarasa <miguel.sarasa@dieffenbacher.de>
2025-10-15 11:36:42 +0200

    cleanup

--- lua/miguel/keymaps.lua
+++ lua/miguel/keymaps.lua
@@ -253,6 +253,5 @@ pcall(vim.keymap.del, 'i', '<C-K>', {buffer=true}) -- pcall because blink keeps 
 
 
 -- Colortheme
-current_colorscheme = "shades_of_purple"
 vim.keymap.set('n', '<leader>cs', Shadesofpurple)
 vim.keymap.set('n', '<leader>cb', ':colorscheme bamboo<cr>')
