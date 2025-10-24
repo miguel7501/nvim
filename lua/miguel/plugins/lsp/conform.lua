63210884fc9df008e07e064e17809204fd4d72ca
Miguel Sarasa <miguel.sarasa@dieffenbacher.de>
2025-10-24 08:37:02 +0200

    svelte formatting stuff

--- lua/miguel/plugins/lsp/conform.lua
+++ lua/miguel/plugins/lsp/conform.lua
@@ -9,12 +9,39 @@ return {
                 sql = { "sqruff" },
                 mysql = { "sqruff" },
                 json = { "jq" },
+                svelte = { lsp_format = "prefer" },
+                javascript = { "prettier", lsp_format = "fallback" },
+                typescript = { "prettier", lsp_format = "fallback" },
             },
         }
-        require("conform").formatters.ruff_format = { prepend_args = { [[--config]], [[line-length=100]] }}
+        require("conform").formatters.ruff_format = { prepend_args = { [[--config]], [[line-length=100]] } }
         require("conform").formatters.sqruff = {
-            prepend_args = { "--config", vim.fn.stdpath("config").."/tool_configs/sqruff.sqruff" },
+            prepend_args = { "--config", vim.fn.stdpath("config") .. "/tool_configs/sqruff.sqruff" },
             require_cwd = false,
         }
+        require("conform").formatters.prettier = { prepend_args = { [[--tabWidth=2]], [[--printWidth=200]] } }
+
+        vim.api.nvim_create_autocmd(
+            { "BufWritePre" },
+            {
+                pattern = {"*.svelte"},
+                callback = function(args)
+                    -- vim.print("BufWritePre called with ", args)
+                    vim.lsp.buf.format()
+                end
+            }
+        )
+        vim.api.nvim_create_autocmd(
+            { "BufWritePre" },
+            {
+                pattern = {"*.ts"},
+                callback = function(args)
+                    -- vim.print("BufWritePost called with ", args)
+                    vim.cmd[[%!prettier --stdin-filepath %]]
+                end
+            }
+        )
+
+
     end
 }
