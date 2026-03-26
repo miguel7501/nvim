c884e3078a9fbd13b84bfcef8f582897cdef8f46
Miguel Sarasa <miguel.sarasa@dieffenbacher.de>
2026-03-26 08:18:30 +0100

    update plugins

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ if not vim.g.vscode then
     vim.g.mapleader = 's'
     vim.g.maplocalleader = 's'
     ---@type boolean
-    Sad = vim.system({"which", "wslview"}):wait().code == 0
+    Sad = vim.fn.has("wsl") == 1
     require("miguel.settings")
     require("miguel.lazy")
     require("miguel.lsp")
