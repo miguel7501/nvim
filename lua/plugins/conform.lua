d6a2e05b0684c362927a4d2b452adce4c1416ab8
Miguel Sarasa <miguel.sarasa@dieffenbacher.de>
2026-05-07 16:28:13 +0200

    add formatting back in

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup {
+            formatters_by_ft = {
+                python = {"ruff_format"}
+            }
+        }
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end
+}
